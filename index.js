932f8f92a70dd3028e7ef6c0e3e7aae0f099e72f
Federica <f.tamerisco@gmail.com>
2024-03-07 16:11:03 +0100

    diciamo versione finale
    mancano dettaglini

--- index.js
+++ index.js
@@ -112,6 +112,22 @@ if(themeMode == "true"){
 //
 //
 //
+//name animation
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+    .add({
+        targets: '.ml16 .letter',
+        translateY: [-100,0],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el, i) => 30 * i
+    });
+
+//
+//
+//
 //automatic calculation of age
 function calcYears(birthday) {
     var today = new Date();
